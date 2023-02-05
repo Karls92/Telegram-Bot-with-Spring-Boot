f2261af0363e5ae2de59f1b87e3366e9bd736566
Karls92 <nbravoalcala@gmail.com>
2023-02-05 15:45:52 -0400

    Telegram Bot App Version 1.0 - First project configuration

---
+++ src/test/java/com/softwaredevone/telegramBot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwaredevone.telegramBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
