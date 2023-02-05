edce6d17a7c333d88c5e5699b11134748050923f
Karls92 <nbravoalcala@gmail.com>
2023-02-05 17:00:55 -0400

    Service layer for Telegram bot has been created

---
+++ src/main/java/com/softwaredevone/telegramBot/service/TelegramBoot.java
@@ -0,0 +1,43 @@
+package com.softwaredevone.telegramBot.service;
+
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Service
+public class TelegramBoot extends TelegramLongPollingBot {
+	
+	@Override
+	public void onUpdateReceived(Update update) {
+		//get telegram user message
+		final String messageFromUser = update.getMessage().getText();
+		
+		//testing the received message
+		System.out.println("The User has written: " + messageFromUser);
+		
+		final long chatId = update.getMessage().getChatId();
+		
+		// send a response message to the user
+		SendMessage responseMessage = new SendMessage();
+		responseMessage.setChatId(chatId);
+		responseMessage.setText("Thanks for contact us! I'm softwareDevOne, your developer friend. My Team and I are here for helping you with your projects. Tell me more about your project and its requirements.");
+		
+		try {
+			execute(responseMessage);
+		} catch(TelegramApiException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	@Override
+	public String getBotUsername() {
+		return "";
+	}
+	
+	@Override
+	public String getBotToken() {
+		return "";
+	}
+}
